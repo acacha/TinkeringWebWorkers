e0d89a7e7b083ed3444d6f5105701dfcbbb6d492
Sergi Tur Badenas <sergiturbadenas@gmail.com>
2017-04-25 20:03:13 +0200

    Initial version

---
+++ src/components/Counter.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <p>Count numbers: {{ counter }}</p>
+    <button @click="startWorker">Start Worker</button>
+    <button @click="stopWorker()">Stop Worker</button>
+  </div>
+
+</template>
+
+<script>
+
+var w
+
+export default {
+  name: 'counter',
+  data () {
+    return {
+      counter: 0
+    }
+  },
+  methods: {
+    startWorker: function () {
+      console.log('start Worker')
+      if (typeof (Worker) !== 'undefined') {
+        if (typeof (w) === 'undefined') {
+          w = new Worker('/static/js/counter.js')
+        }
+        w.onmessage = function (event) {
+          this.counter = event.data
+        }
+      } else {
+        console.log('Sorry! No Web Worker support.')
+      }
+    },
+    stopWorker: function () {
+      console.log('stop Worker')
+      w.terminate()
+      w = undefined
+    }
+  }
+}
+</script>
+
